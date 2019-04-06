a47f89a649eac0c7b091df2d94661e867e5a5ef2
Leonid Volokitin <l.volokitin@macte.pro>
2019-04-07 02:36:38 +0300

    Added entity editor open  button

---
+++ dist/build/static/js/ee.btn.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+	document.body.innerHTML += '<div id="entity_editor" style="position:absolute;padding:0;margin:0;top:0;left:0;width:100%;height:100%;background:rgba(255,255,255,1);z-index:10;display:none;"></div>';
+	document.body.innerHTML += '<button onClick="document.getElementById(\'entity_editor\').style.display=\'block\'" type="button" class="ant-btn ant-btn-danger" style="position: fixed; top: 40px; right: 20px;">Open Entity Editor</button>';
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://cdn.jsdelivr.net/gh/leonvv/bitrix24-entity-editor@latest/dist/build/static/js/ee.btn.js';
+	head.appendChild(script);
+}
